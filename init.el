4f00c52f10c10a07d3a1fd106c86de3dab4ce522
Trevor Arjeski <tmarjeski@gmail.com>
2024-05-25 14:02:46 +0300

    add gnus gmail config

--- init.el
+++ init.el
@@ -105,6 +105,9 @@
           ((shift) . 1)
           ((control) . nil))                    ; Mouse wheel scrolling
         scroll-preserve-screen-position 1       ; PgUp/PgDown hold
+        gnus-init-file (expand-file-name
+                        "gnus/gnus.el"
+                        user-emacs-directory)
         send-mail-function
         'message-send-mail-with-sendmail)       ; Use sendmail
   (defalias 'yes-or-no #'y-or-n-p)              ; Easier question
@@ -206,7 +209,7 @@
          ("M-s e" . consult-isearch-history)
          ("M-s g" . consult-grep)
          ("M-s k" . consult-keep-lines)
-         ("M-s l" . consult-line)
+         ("C-s" . consult-line)
          ("M-s r" . consult-ripgrep)
          ("M-s u" . consult-focus-lines)
          ("M-y" . consult-yank-pop)                ;; orig. yank-pop
