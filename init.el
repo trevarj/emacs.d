20d7838688ddb394f11d5d39ac814875f0b2efb5
Trevor Arjeski <tmarjeski@gmail.com>
2024-09-23 20:03:50 +0300

    sudo save function

--- init.el
+++ init.el
@@ -36,6 +36,12 @@
     (when buffer-file-name
       (setq header-line-format '("%f"))))
 
+  (defun sudo-save ()
+    (interactive)
+    (if (not buffer-file-name)
+        (write-file (concat "/sudo:root@localhost:" (ido-read-file-name "File:")))
+      (write-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
   :hook
   ((emacs-startup . display-startup-time)
    (buffer-list-update . header-line-file-path))
