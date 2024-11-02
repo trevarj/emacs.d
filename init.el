1a717ce74f2fa1f199f1ddd85407c1e79b631106
Trevor Arjeski <tmarjeski@gmail.com>
2024-11-02 15:46:56 +0300

    convert to using :custom

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
   (set-face-attribute 'default nil :family "Iosevka JBM" :height 150 :weight 'medium)
   (set-face-attribute 'fixed-pitch nil :font "Iosevka JBM" :height 150)
   (set-face-attribute 'variable-pitch nil :family "Noto Sans")
-  (setq use-default-font-for-symbols nil)
+  (setopt use-default-font-for-symbols nil)
   ;; https://github.com/ryanoasis/nerd-fonts/wiki/Glyph-Sets-and-Code-Points
   (defvar nerdfont-code-points
     '((#xE6FA . #xE6B2)
@@ -97,53 +97,53 @@
   Mono")))
 
   ;; Miscellaneous Options
-  (setq-default fill-column 80                   ; 80 width pages
-                auto-fill-function 'do-auto-fill ; always autofill
-                indent-tabs-mode nil             ; Use spaces only
-                major-mode                       ; Guess major mode from file name
-                (lambda ()
-                  (unless buffer-file-name
-                    (let ((buffer-file-name (buffer-name)))
-                      (set-auto-mode)))))
-  (setq
-   user-full-name "Trevor Arjeski"
-   user-mail-address "tmarjeski@gmail.com"
-   use-package-always-defer t              ; always defer packages, use :demand instead
-   custom-file "/tmp/custom.el"            ; customization file
-   desktop-load-locked-desktop 'check-pid  ; load if lock pid doesn't exist
-   display-line-numbers-grow-only t        ; Never shrink the linum width
-   display-line-numbers-width-start t      ; Calculate linum width at start
-   confirm-kill-emacs nil
-   use-dialog-box nil                      ; Bye
-   use-short-answers t                     ; y/n
-   global-auto-revert-non-file-buffers t   ; Auto-refresh buffers like dired
-   auto-revert-verbose nil                 ; But silence it
-   enable-recursive-minibuffers t          ; Recursive mini-buffers
-   inhibit-startup-message t               ; No startup screen
-   scroll-step 1                           ; Vim style scrolling
-   scroll-margin 10                        ; Vim style scroll off
-   fill-column 80                          ; Line width 80 chars
-   comment-auto-fill-only-comments t       ; Autofill comments only
-   mode-line-front-space nil               ; Nicer -nw mode line
-   mode-line-end-spaces nil                ; ^
-   window-divider-default-right-width 16   ; Padding between splits
-   backup-directory-alist '(("." . "~/.cache/emacs/backups"))
-   auto-save-file-name-transforms '((".*" "~/.cache/emacs/saves/" t))
-   undo-limit 67108864                     ; Undo limit of 64mb.
-   undo-strong-limit 100663296             ;               96mb.
-   undo-outer-limit 1006632960             ;               960mb.
-   eldoc-echo-area-use-multiline-p nil
-   epg-pinentry-mode 'loopback             ; pinentry on minibuffer
-   mouse-wheel-progressive-speed nil
-   mouse-wheel-scroll-amount
+  :custom
+  (user-full-name "Trevor Arjeski")
+  (user-mail-address "tmarjeski@gmail.com")
+  (use-package-always-defer t)              ; always defer packages, use :demand instead
+  (custom-file "/tmp/custom.el")            ; customization file
+  (desktop-load-locked-desktop 'check-pid)  ; load if lock pid doesn't exist
+  (display-line-numbers-grow-only t)        ; Never shrink the linum width
+  (display-line-numbers-width-start t)      ; Calculate linum width at start
+  (confirm-kill-emacs nil)
+  (use-dialog-box nil)                      ; Bye
+  (use-short-answers t)                     ; y/n
+  (global-auto-revert-non-file-buffers t)   ; Auto-refresh buffers like dired
+  (auto-revert-verbose nil)                 ; But silence it
+  (enable-recursive-minibuffers t)          ; Recursive mini-buffers
+  (inhibit-startup-message t)               ; No startup screen
+  (scroll-step 1)                           ; Vim style scrolling
+  (scroll-margin 10)                        ; Vim style scroll off
+  (fill-column 80)                          ; Line width 80 chars
+  (comment-auto-fill-only-comments t)       ; Autofill comments only
+  (mode-line-front-space nil)               ; Nicer -nw mode line
+  (mode-line-end-spaces nil)                ; ^
+  (window-divider-default-right-width 16)   ; Padding between splits
+  (backup-directory-alist '(("." . "~/.cache/emacs/backups")))
+  (auto-save-file-name-transforms '((".*" "~/.cache/emacs/saves/" t)))
+  (undo-limit 67108864)                     ; Undo limit of 64mb.
+  (undo-strong-limit 100663296)             ;               96mb.
+  (undo-outer-limit 1006632960)             ;               960mb.
+  (eldoc-echo-area-use-multiline-p nil)
+  (epg-pinentry-mode 'loopback)             ; pinentry on minibuffer
+  (mouse-wheel-progressive-speed nil)
+  (mouse-wheel-scroll-amount
    '(3
      ((shift) . 1)
-     ((control) . nil))                    ; Mouse wheel scrolling
-   scroll-preserve-screen-position 1       ; PgUp/PgDown hold
-   gnus-init-file (expand-file-name
+     ((control) . nil)))                    ; Mouse wheel scrolling
+  (scroll-preserve-screen-position 1)       ; PgUp/PgDown hold
+  (gnus-init-file (expand-file-name
                    "gnus/gnus.el"
-                   user-emacs-directory)
-   send-mail-function 'message-send-mail-with-sendmail) ; Use sendmail
+                   user-emacs-directory))
+  (send-mail-function 'message-send-mail-with-sendmail) ; Use sendmail
+  (fill-column 80)                   ; 80 width pages
+  (auto-fill-function 'do-auto-fill) ; always autofill
+  (indent-tabs-mode nil)             ; Use spaces only
+  (major-mode                       ; Guess major mode from file name
+   (lambda ()
+     (unless buffer-file-name
+       (let ((buffer-file-name (buffer-name)))
+         (set-auto-mode)))))
   ;; Generic keybindings
   :bind
   (("C-x b" . ibuffer)
@@ -172,11 +172,12 @@
   :preface
   (defun get-doom-theme-color (name)
     (car (alist-get name doom-themes--colors)))
+  :custom
+  (doom-themes-enable-bold t)
+  (doom-themes-enable-italic nil)
+  (doom-nord-padded-modeline t)
+  (doom-nord-brighter-modeline t)
   :config
-  (setq doom-themes-enable-bold t
-        doom-themes-enable-italic nil
-        doom-nord-padded-modeline t
-        doom-nord-brighter-modeline t)
   (load-theme 'doom-nord t)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config)
@@ -304,7 +305,7 @@
   (defun corfu-enable-always-in-minibuffer ()
     "Enable Corfu in the minibuffer if Vertico is not active."
     (unless (or (bound-and-true-p vertico--input)
-                (eq (current-local-map) read-passwd-map))
+                (eq (current-local-map) 'read-passwd-map))
       ;; (setq-local corfu-auto nil) ;; Enable/disable auto completion
       (setq-local corfu-echo-delay nil ;; Disable automatic echo and popup
                   corfu-popupinfo-delay nil)
@@ -313,14 +314,15 @@
   (global-corfu-mode)
   :hook
   ('minibuffer-setup . #'corfu-enable-always-in-minibuffer)
+  :custom
+  (corfu-auto t)
+  (corfu-popupinfo-delay 0)
+  (corfu-quit-no-match 'separator)
+  (corfu-preview-current nil)
+  (tab-always-indent 'complete)
   :config
   (corfu-popupinfo-mode)
   (add-to-list 'corfu--frame-parameters '(internal-border-width . 2))
-  (setq corfu-auto t
-        corfu-popupinfo-delay 0
-        corfu-quit-no-match 'separator
-        corfu-preview-current nil
-        tab-always-indent 'complete)
   (set-face-background 'corfu-border (get-doom-theme-color 'orange))
   :bind
   (:map corfu-map
@@ -346,8 +348,8 @@
   :demand
   :custom
   (treesit-auto-install 'prompt)
-  :config
   (treesit-auto-add-to-auto-mode-alist 'all)
+  :config
   (global-treesit-auto-mode))
 
 ;; Whitespace handling
@@ -396,9 +398,9 @@
 
 ;; Git
 (use-package magit
-  :config
-  (setq magit-define-global-key-bindings 'recommended
-        magit-diff-refine-hunk t))
+  :custom
+  (magit-define-global-key-bindings 'recommended)
+  (magit-diff-refine-hunk t))
 
 ;; Git modes
 (use-package git-modes)
@@ -458,22 +460,20 @@
     "Safely turn on `parinfer-rust-mode'."
     (electric-pair-mode nil) ; conflicts with parinfer
     (parinfer-rust-mode))
+  :custom
+  (parinfer-rust-troublesome-modes '())
   :config
-  (setq
-   parinfer-rust-troublesome-modes '())
   (set-face-attribute
    'parinfer-rust-dim-parens nil
    :foreground (cadr (assoc 'base6 doom-themes--colors)))
   :hook
-  ((emacs-lisp-mode sly-mode geiser-mode) . safe-parinfer-rust-mode))
+  ((emacs-lisp-mode sly-mode geiser-mode) . #'safe-parinfer-rust-mode))
 
 ;; Rust
 (use-package rust-mode
-  :init
-  (setq rust-mode-treesitter-derive t)
-  :config
-  (setq rust-format-on-save t)
-  (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+  :custom
+  (rust-mode-treesitter-derive t)
+  (rust-format-on-save t))
 
 ;; Guile
 (use-package geiser-guile
@@ -482,8 +482,8 @@
 
 ;; Common Lisp
 (use-package sly
-  :config
-  (setq inferior-lisp-program (executable-find "sbcl")))
+  :custom
+  (inferior-lisp-program (executable-find "sbcl")))
 
 ;; Markdown
 (use-package markdown-mode)
@@ -508,7 +508,13 @@
       (message "hidden fools: %s" (if hidden-fools "on" "off"))
       (set-buffer-modified-p t)))
   :config
-  (load-library (expand-file-name "secrets.el.gpg" user-emacs-directory))
+  (load-file (expand-file-name "secrets.el.gpg" user-emacs-directory))
+  (setopt erc-modules (seq-union '(nicks scrolltobottom spelling) erc-modules)
+          erc-nicks-colors (mapcar 'get-doom-theme-color
+                                   '(orange yellow teal blue dark-blue cyan violet)))
+  ;; for more "themed" erc-nicks-colors
+  (setq erc-nicks--create-pool-function 'erc-nicks--create-culled-pool)
+
   ;; Fix for restoring query buffers with self-messages
   (advice-add #'erc-login
               :before (lambda ()
@@ -516,39 +522,37 @@
                         (erc-server-send "CAP END")))
   ;; Clear out query bufs when using using `AutoClearQueryBuffer = false`
   (add-to-list 'erc-kill-buffer-hook 'erc-clear-query-buffer)
-  (setq
-   erc-server "orangepi"
-   erc-port "7777"
-   erc-nick "trev"
-   erc-user-full-name "trev"
-   erc-prompt 'erc-prompt-format
-   erc-prompt-format (propertize "%n:" 'font-lock-face 'erc-input-face)
-   erc-track-exclude-types '("JOIN" "NICK" "QUIT" "MODE" "AWAY" "PART"
-                             "353" "324" "332" "329" "333" "477")
-   erc-hide-list '("JOIN" "NICK" "QUIT" "MODE" "AWAY" "PART" "353")
-   erc-track-exclude-server-buffer t
-   erc-track-exclude '("#emacs" "#systemcrafters-live" "*status")
-   ;; works with bug#67767 v2 patch
-   erc-nicks-track-faces t
-   erc-fools my/erc-fools
-   erc-fool-highlight-type 'all
-   erc-current-nick-highlight-type 'all
-   erc-insert-timestamp-function 'erc-insert-timestamp-left
-   erc-timestamp-format "%H:%M"
-   erc-nicks-colors (mapcar 'get-doom-theme-color
-                            '(orange yellow teal blue dark-blue cyan violet))
-   ;; for above to work
-   erc-nicks--create-pool-function 'erc-nicks--create-culled-pool
-   erc-receive-query-display 'bury
-   erc-fill-function 'erc-fill-static
-   erc-fill-static-center 16)
-  (setopt erc-modules
-          (seq-union '(nicks scrolltobottom spelling) erc-modules))
-  ;; keep erc-track faces uniform, ex. when someone sends a url
-  (delete '(erc-button erc-default-face) erc-track-faces-priority-list)
   (erc-spelling-mode)
   (erc-scrolltobottom-mode)
-  (defun erc-match-directed-at-fool-p (msg) nil)
+  (defun erc-match-directed-at-fool-p (_msg) nil)
+  :custom
+  (erc-server "orangepi")
+  (erc-port "7777")
+  (erc-nick "trev")
+  (erc-user-full-name "trev")
+  (erc-prompt 'erc-prompt-format)
+  (erc-prompt-format (propertize "%n:" 'font-lock-face 'erc-input-face))
+  (erc-track-exclude-types '("JOIN" "NICK" "QUIT" "MODE" "AWAY" "PART")
+                           "353" "324" "332" "329" "333" "477")
+  (erc-hide-list '("JOIN" "NICK" "QUIT" "MODE" "AWAY" "PART" "353"))
+  (erc-track-exclude-server-buffer t)
+  (erc-track-exclude '("#emacs" "#systemcrafters-live" "*status"))
+  ;; works with bug#67767 v2 patch
+  (erc-nicks-track-faces t)
+  (erc-fools my/erc-fools)
+  (erc-fool-highlight-type 'all)
+  (erc-current-nick-highlight-type 'all)
+  (erc-insert-timestamp-function 'erc-insert-timestamp-left)
+  (erc-timestamp-format "%H:%M")
+  (erc-receive-query-display 'bury)
+  (erc-fill-function 'erc-fill-static)
+  (erc-fill-static-center 16)
+  (erc-track-faces-priority-list
+   '(erc-nick-msg-face erc-direct-msg-face
+                       (erc-button-nick-default-face erc-nick-default-face)
+                       (erc-button-nick-default-face erc-default-face)
+                       erc-default-face erc-action-face erc-fool-face
+                       erc-notice-face erc-input-face erc-prompt-face))
   :hook
   ((erc-mode . (lambda ()
                  (auto-fill-mode -1)
@@ -565,14 +569,15 @@
    ("C-c -" . 'erc-toggle-fools)))
 
 (use-package elfeed
-  :config
-  (load-library (expand-file-name "secrets.el.gpg" user-emacs-directory))
-  (setq elfeed-feeds my/elfeed-feeds)
-
+  :preface
   (defun elfeed-show-visit-eww ()
     (interactive)
     (let ((browse-url-browser-function #'eww-browse-url))
       (elfeed-show-visit)))
+  :config
+  (load-library (expand-file-name "secrets.el.gpg" user-emacs-directory))
+  :custom
+  (elfeed-feeds my/elfeed-feeds)
   :bind
   (("C-c @" . #'elfeed)
    :map elfeed-show-mode-map
@@ -585,11 +590,16 @@
          :branch "local-cookie")
   :config
   (load-library (expand-file-name "secrets.el.gpg" user-emacs-directory))
-  (setq leetcode-session-cookie my/leetcode-session-cookie
-        leetcode-prefer-language "cpp"
-        leetcode-save-solutions t
-        leetcode-directory "~/Workspace/leetcode/"))
+  :custom
+  (leetcode-session-cookie my/leetcode-session-cookie)
+  (leetcode-prefer-language "cpp")
+  (leetcode-save-solutions t)
+  (leetcode-directory "~/Workspace/leetcode/"))
 
 (provide 'init)
+
+;; Local Variables:
+;; byte-compile-warnings: (not unresolved free-vars)
+;; End:
 
 ;;; init.el ends here
