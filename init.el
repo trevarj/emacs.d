7b081bd2c88b4fcd5c61e2909a9acb8a8194e925
Trevor Arjeski <tmarjeski@gmail.com>
2024-11-11 10:47:53 +0300

    move aoc.el to my custom packages

--- init.el
+++ init.el
@@ -574,10 +574,6 @@
   (leetcode-directory "~/Workspace/leetcode/"))
 
 (use-package aoc
-  :vc t
-  :load-path "~/Workspace/advent-of-code"
-  ;; (:url "https://github.com/trevarj/advent-of-code"
-  ;;       :rev "9c5bd70")
   :load my-secrets
   :config
   (setopt aoc-session-cookie my/aoc-session-cookie
