70586f02b35a0543df8adbb31b3889c5311c59e9
Trevor Arjeski <tmarjeski@gmail.com>
2025-10-09 13:08:41 +0300

    cleanup early init

--- early-init.el
+++ early-init.el
@@ -4,15 +4,16 @@
 
 ;;; Code:
 
-;; Startup hacks
-(setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6
-      vc-handled-backends '(Git))
-
-;; Disable site files
 (setq
+ ;; Startup hacks
+ gc-cons-threshold most-positive-fixnum
+ gc-cons-percentage 0.6
+ vc-handled-backends '(Git)
+ ;; Disable site files
  inhibit-default-init t
- site-run-file nil)
+ site-run-file nil
+ ;; Don't forget to run `package-quickstart-refresh'!
+ package-quickstart t)
 
 ;; After Emacs has completely started, reset the values to more sensible ones.
 (add-hook
@@ -24,24 +25,25 @@
            gc-cons-percentage 0.1
            file-name-handler-alist orig-file-name-handler-alist))))
 
-(setq package-quickstart t)                 ; Don't forget to run `package-quickstart-refresh'!
 (scroll-bar-mode -1)                        ; Disable visible scrollbar
 (tool-bar-mode -1)                          ; Disable the toolbar
 (menu-bar-mode -1)                          ; Disable the menubar
 (tooltip-mode -1)                           ; Disable tooltips
 (modify-all-frames-parameters
  '((internal-border-width . 40)))           ; Frame padding
-(setq-default left-fringe-width 4
-              left-margin-width 1
-              right-fringe-width 4
-              right-margin-width 1)
+(setq-default
+ left-fringe-width 4
+ left-margin-width 1
+ right-fringe-width 4
+ right-margin-width 1)
 
 
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
 ;; larger than the system default.
-(setq frame-inhibit-implied-resize t
-      frame-resize-pixelwise t)
+(setq
+ frame-inhibit-implied-resize t
+ frame-resize-pixelwise t)
 
 (provide 'early-init)
 
