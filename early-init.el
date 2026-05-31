c8da82b5371054267c6a637884657b75111fa704
Trevor Arjeski <tmarjeski@gmail.com>
2026-05-31 08:55:39 +0000

    early-init GC lag fix and cleanup

--- early-init.el
+++ early-init.el
@@ -4,45 +4,51 @@
 
 ;;; Code:
 
-(setq
- ;; Startup hacks
- gc-cons-threshold most-positive-fixnum
- gc-cons-percentage 0.6
- vc-handled-backends '(Git)
- ;; Disable site files
- inhibit-default-init t
- site-run-file nil
- user-lisp-directory (expand-file-name "lisp" user-emacs-directory))
+;;; Startup
 
-;; After Emacs has completely started, reset the values to more sensible ones.
+;; Defer garbage collection during startup, then restore a high enough
+;; threshold to avoid visible pauses during sustained input in a loaded session.
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
 (add-hook
- 'emacs-startup-hook
+ 'after-init-hook
  (lambda ()
-   (setq gc-cons-threshold (* 16 1024 1024) ; 16mb
-         gc-cons-percentage 0.1)))
+   (setq gc-cons-threshold (* 128 1024 1024)
+         gc-cons-percentage 0.2)))
 
-(scroll-bar-mode -1)                        ; Disable visible scrollbar
-(tool-bar-mode -1)                          ; Disable the toolbar
-(menu-bar-mode -1)                          ; Disable the menubar
-(tooltip-mode -1)                           ; Disable tooltips
+;; Keep startup deterministic and avoid scanning site files.
+(setq inhibit-default-init t
+      site-run-file nil
+      user-lisp-directory (expand-file-name "lisp" user-emacs-directory))
 
-;; Resizing the Emacs frame can be a terribly expensive part of changing the
-;; font. By inhibiting this, we easily halve startup times with fonts that are
-;; larger than the system default.
-(setq
- frame-inhibit-implied-resize t
- frame-resize-pixelwise t
- window-resize-pixelwise t)
+;; Restrict VC work during early startup.
+(setq vc-handled-backends '(Git))
 
-;; Other performance tweaks
-;; https://emacsredux.com/blog/2026/04/07/stealing-from-the-best-emacs-configs/
+;;; UI Defaults
+
+;; Disable chrome before the initial frame is created.
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(tooltip-mode -1)
+
+;; Avoid expensive implied frame resizes when fonts and toolkit widgets settle.
+(setq frame-inhibit-implied-resize t
+      frame-resize-pixelwise t
+      window-resize-pixelwise t)
+
+;;; Responsiveness
+
+;; Bias redisplay and fontification toward accepting new input quickly.
 (setq-default bidi-paragraph-direction 'left-to-right)
-(setq bidi-inhibit-bpa t)
-(setq redisplay-skip-fontification-on-input t)
-(setq read-process-output-max (* 4 1024 1024)) ; 4MB
 (setq-default cursor-in-non-selected-windows nil)
-(setq highlight-nonselected-windows nil)
-(setq pgtk-wait-for-event-timeout nil)
+
+(setq bidi-inhibit-bpa t
+      redisplay-skip-fontification-on-input t
+      read-process-output-max (* 4 1024 1024)
+      highlight-nonselected-windows nil
+      pgtk-wait-for-event-timeout 0.1)
 
 (provide 'early-init)
 
